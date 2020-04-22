45245908911c3d520a406c2884752baed46f1b81
arsalanrafique <arsalanrafique@hollandandbarrett.com>
2020-04-22 19:42:41 +0100

    fix render 4 & 5 stars reviews

--- my-app/src/component/PersonalReviews/PersonalReviews.js
+++ my-app/src/component/PersonalReviews/PersonalReviews.js
@@ -5,6 +5,7 @@ const PersonalReviews = () => {
   const data = useContext(Context);
   const { personalReviews = {} } = data;
   const { reviews = null } = personalReviews;
+  console.log(reviews);
   return (
     reviews && (
       <>
@@ -18,13 +19,16 @@ const PersonalReviews = () => {
           </thead>
           <tbody>
             {reviews &&
-              reviews.map((item, idx) => (
-                <tr key={idx}>
-                  <td>{item.stars}</td>
-                  <td>{item.consumer.displayName}</td>
-                  <td>{item.text}</td>
-                </tr>
-              ))}
+              reviews.map((item, idx) => {
+                 console.log(item.stars === 5)
+                    return (item.stars === 5 || item.stars === 4) && (
+                        <tr key={idx}>
+                        <td>{item.stars}</td>
+                        <td>{item.consumer.displayName}</td>
+                        <td>{item.text}</td>
+                      </tr>
+                    )
+              })}
           </tbody>
         </table>
       </>
